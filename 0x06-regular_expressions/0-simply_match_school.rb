bed169e211b75fd502881f6e3655cd537fa50bde
wacherawaweru <wacheram22@gmail.com>
2024-05-29 12:57:55 -0700

    corrections

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,5 +1,17 @@
 #!/usr/bin/env ruby
+
 # This script accepts one argument and passes it to a regular expression matching method
 # The regular expression must match "School"
 
-puts ARGV[0].scan(/School/).join
+# Extract the argument provided
+input_string = ARGV[0]
+
+# Define the regular expression pattern to match "School"
+pattern = /School/
+
+# Check if the input string matches the pattern
+if input_string.match?(pattern)
+  puts input_string
+else
+  puts "No match"
+end
--- 0x06-regular_expressions/2-repetition_token_1.rb
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -1,5 +1,17 @@
 #!/usr/bin/env ruby
-# This script accepts one argument and passes it to a regular expression matching method
-# The regular expression matches specific patterns involving repetition tokens
 
-puts ARGV[0].scan(/h(b?t)n/).join
+# This script accepts one argument and passes it to a regular expression matching method
+# The regular expression matches "hbtn" followed by one or more occurrences of "t"
+
+# Extract the argument provided
+input_string = ARGV[0]
+
+# Define the regular expression pattern
+pattern = /hbt+n/
+
+# Check if the input string matches the pattern
+if input_string.match?(pattern)
+  puts input_string
+else
+  puts "No match"
+end

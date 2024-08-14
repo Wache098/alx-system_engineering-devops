451f2d606ae2e2d330de3239ea526c9c6067c150
wacherawaweru <wacheram22@gmail.com>
2024-08-14 06:46:26 -0700

    corrected

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,41 +1,27 @@
 #!/usr/bin/python3
-import json
-import requests
-
-
-def fetch_and_format_data():
-    """Fetch users and tasks from the API and format them into a dictionary."""
-    users_response = requests.get("https://jsonplaceholder.typicode.com/users")
-    tasks_response = requests.get("https://jsonplaceholder.typicode.com/todos")
-
-    users = users_response.json()
-    tasks = tasks_response.json()
-
-    all_data = {}
-
-    for user in users:
-        user_id = user['id']
-        username = user['username']
-        user_tasks = [
-            {"username": username, "task": task['title'], "completed": task['completed']}
-            for task in tasks if task['userId'] == user_id
-        ]
-        all_data[user_id] = user_tasks
-
-    return all_data
-
-
-def save_to_file(data, filename="todo_all_employees.json"):
-    """Save the formatted data to a JSON file."""
-    with open(filename, "w") as json_file:
-        json.dump(data, json_file)
-
-
-def main():
-    """Main entry point of the script."""
-    data = fetch_and_format_data()
-    save_to_file(data)
-
+"""Exports data in the JSON format"""
 
 if __name__ == "__main__":
-    main()
+
+    import json
+    import requests
+    import sys
+
+    users = requests.get("https://jsonplaceholder.typicode.com/users")
+    users = users.json()
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+    todos = todos.json()
+    todoAll = {}
+
+    for user in users:
+        taskList = []
+        for task in todos:
+            if task.get('userId') == user.get('id'):
+                taskDict = {"username": user.get('username'),
+                            "task": task.get('title'),
+                            "completed": task.get('completed')}
+                taskList.append(taskDict)
+        todoAll[user.get('id')] = taskList
+
+    with open('todo_all_employees.json', mode='w') as f:
+        json.dump(todoAll, f)
